c6672590b15c6a938535395ef6c2f39c930ae0e4
Andrey Hihlovskiy <akhikhl@gmail.com>
2013-06-09 22:59:21 +0200

    renamed example program to gineSimpleExample, synchronized code to gradle-onejar plugin

---
+++ libs/gino-plugin/src/main/groovy/gino/GinoAppPlugin.groovy
@@ -0,0 +1,19 @@
+package gino
+
+import org.gradle.api.*
+import org.gradle.api.plugins.*
+import org.gradle.api.tasks.*
+
+class GinoAppPlugin implements Plugin<Project> {
+
+  void apply(final Project project) {
+
+    project.apply plugin: "onejar"
+
+    project.jar {
+      manifest { attributes "Main-Class": "gino.Main" }
+    }
+
+    project.dependencies { compile "org.akhikhl.gino:gino-runner:0.0.1" }
+  }
+}
