d457f6b3d3d01e7d3440c1cb0b9824777783df5c
Andrey Khikhlovskiy <ahi@startext.de>
2013-06-10 11:44:22 +0200

    Implemented Gino-Javascript manifest entry

--- libs/gino-runner/src/main/java/gino/Main.java
+++ libs/gino-runner/src/main/java/gino/Main.java
@@ -1,6 +1,5 @@
 package gino;
 
-import java.io.InputStream;
 import java.net.URL;
 import java.util.Enumeration;
 import java.util.jar.Manifest;
@@ -12,22 +11,18 @@ public class Main {
   private static final Logger logger = org.slf4j.LoggerFactory.getLogger(Main.class);
 
   public static void main(String[] args) throws Exception {
-    System.out.println("DBG about to show manifest entries");
-    ClassLoader classLoader = Main.class.getClassLoader();
-    Enumeration<URL> resources = classLoader.getResources("META-INF/MANIFEST.MF");
+    Enumeration<URL> resources = Main.class.getClassLoader().getResources("META-INF/MANIFEST.MF");
+    String ginoJavascript = "main.js";
     while (resources.hasMoreElements()) {
-      URL url = resources.nextElement();
-      String manifestStr = org.apache.commons.io.IOUtils.toString(url, "UTF-8");
-      if (manifestStr.contains("Gino-Javascript"))
-        System.out.println("DBG manifest: " + manifestStr);
-      InputStream stm = url.openStream();
-      Manifest manifest = new Manifest(stm);
+      Manifest manifest = new Manifest(resources.nextElement().openStream());
       for (Object key : manifest.getMainAttributes().keySet()) {
         String attrName = key.toString();
-        if ("Gino-Javascript".equals(attrName))
-          System.out.println("  key=" + attrName + ", value=" + manifest.getMainAttributes().getValue(attrName));
+        if ("Gino-Javascript".equals(attrName)) {
+          ginoJavascript = manifest.getMainAttributes().getValue(attrName);
+          break;
+        }
       }
     }
-    Runner.run("main.js", args, Main.class.getClassLoader(), logger);
+    Runner.run(ginoJavascript, args, Main.class.getClassLoader(), logger);
   }
 }
