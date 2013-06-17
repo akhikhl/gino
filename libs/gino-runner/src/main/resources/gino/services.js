9d721e35414f0fd5eebfeb72b01755caa4371ad0
Andrey Khikhlovskiy <ahi@startext.de>
2013-06-17 13:41:51 +0200

    introduced function services.findServiceByName

--- libs/gino-runner/src/main/resources/gino/services.js
+++ libs/gino-runner/src/main/resources/gino/services.js
@@ -4,19 +4,24 @@
       
     let serviceMap = {}, serviceList = [];
 
-    this.registerService = function(serviceName) {
-      if(!(serviceName in serviceMap)) {
-        // first assignment is needed to avoid possible recursions
-        serviceMap[serviceName] = {};
-        let serviceController = tryLoad(serviceName);
-        if(serviceController == null && serviceName.slice(-3) != ".js")
-          serviceController = tryLoad(serviceName + ".js");
-        if(serviceController == null)
-          throw new Error("Service not found: " + serviceName);
-        serviceMap[serviceName] = serviceController;
-        // this is needed to guarantee proper service initialization order
-        serviceList.push({ serviceController: serviceController, serviceName: serviceName });
+    this.disposeServices = function(domains) {
+      // need to destroy services in reverse order
+      for(let i = serviceList.length - 1; i >= 0; i--) {
+        let s = serviceList[i];
+        logger.trace("Stopping service: {}", s.serviceName);
+        if(typeof s.serviceController.dispose == "function") {
+          logger.trace("Disposing service: {}", s.serviceName);
+          let startTime = System.currentTimeMillis();
+          s.serviceController.dispose(domains);
+          logger.trace("Disposed service {} in {} seconds", s.serviceName, (System.currentTimeMillis() - startTime) / 1000);
+        }
       }
+      serviceList = [];
+      serviceMap = {};
+    };
+    
+    this.findServiceByName = function(serviceName) {
+      return serviceMap[serviceName];
     };
     
     this.getServices = function() { for each(service in serviceList) yield service; };
@@ -46,20 +51,19 @@
       }
     };
 
-    this.disposeServices = function(domains) {
-      // need to destroy services in reverse order
-      for(let i = serviceList.length - 1; i >= 0; i--) {
-        let s = serviceList[i];
-        logger.trace("Stopping service: {}", s.serviceName);
-        if(typeof s.serviceController.dispose == "function") {
-          logger.trace("Disposing service: {}", s.serviceName);
-          let startTime = System.currentTimeMillis();
-          s.serviceController.dispose(domains);
-          logger.trace("Disposed service {} in {} seconds", s.serviceName, (System.currentTimeMillis() - startTime) / 1000);
-        }
+    this.registerService = function(serviceName) {
+      if(!(serviceName in serviceMap)) {
+        // first assignment is needed to avoid possible recursions
+        serviceMap[serviceName] = {};
+        let serviceController = tryLoad(serviceName);
+        if(serviceController == null && serviceName.slice(-3) != ".js")
+          serviceController = tryLoad(serviceName + ".js");
+        if(serviceController == null)
+          throw new Error("Service not found: " + serviceName);
+        serviceMap[serviceName] = serviceController;
+        // this is needed to guarantee proper service initialization order
+        serviceList.push({ serviceController: serviceController, serviceName: serviceName });
       }
-      serviceList = [];
-      serviceMap = {};
     };
   }
   
